c82025fdfb00c72edf2dca8bffea7e085bda15f4
Zorin <pikasama@qq.com>
2021-05-25 22:20:46 +0800

    test: extension function test

--- src/modules/extend.ts
+++ src/modules/extend.ts
@@ -1,26 +1,29 @@
-import { mergeMap, Template } from './utils';
+import {Logger, mergeMap, Template} from './utils';
 
 async function extension(template: Template, templateMap: Map<string, Template>) {
-    let mergedFiles: Template['fileMap'];
-    const extendTemplates = template.extends as string[];
-    extendTemplates.push(template.name);
+    // BUG: 暂不支持嵌套的继承
+    let mergeFiles: Template['fileMap'];
+    const mergeTemplateList = template.extends as string[];
+    mergeTemplateList.push(template.name);
 
-    const firstExtendTemplate = templateMap.get(extendTemplates[0]);
-    if (!firstExtendTemplate) {
-        throw `Could not find template ${extendTemplates[0]}`;
+    const firstTemplate = templateMap.get(mergeTemplateList[0]);
+    if (!firstTemplate) {
+        throw `Could not find template ${mergeTemplateList[0]}`;
     }
 
-    extendTemplates.shift();
-    mergedFiles = firstExtendTemplate.fileMap;
+    mergeTemplateList.shift();
+    mergeFiles = firstTemplate.fileMap;
 
-    extendTemplates.map((val) => {
+    mergeTemplateList.map((val) => {
         const templ = templateMap.get(val);
         if (!templ) {
             throw `Could not find template ${val}`;
         }
-        mergedFiles = mergeMap(mergedFiles, templ.fileMap);
+        mergeFiles = mergeMap(mergeFiles, templ.fileMap);
         return undefined;
     });
+
+    Logger.debug(mergeFiles);
 }
 
 export { extension as mergeExtend };
--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,77 @@
 // Just tests
 import * as inquirer from 'inquirer';
 import * as shell from 'shelljs';
-import { awaitHelper, Logger } from './modules/utils';
+import {awaitHelper, Logger, mergeMap, Template} from './modules/utils';
+import { mergeExtend } from "./modules/extend";
 
-(async () => {
-    function test() {
-        throw 'Error Test';
+// const templates: Template[] = [
+//     {
+//         name: 'a',
+//         repo: 'a',
+//         fileMap: new Map([
+//             ['a.js','.']
+//         ]),
+//     },
+//     {
+//         name: 'b',
+//         repo :'b',
+//         fileMap: new Map([
+//             ['b.js','.']
+//         ])
+//     },
+//     {
+//         name: 'c',
+//         repo: 'c',
+//         fileMap: new Map([
+//             ['c.js','.']
+//         ]),
+//         extends: ['a','b']
+//     }
+// ];
+//
+// (async () => {
+//     const [err, res] = await awaitHelper(mergeExtend(templates[2],new Map(templates.map(val => [val.name, val]))));
+// })().catch((err) => Logger.err(err));
+
+// 嵌套依赖
+const obj = {
+    a: {
+        cont: 'a',
+    },
+    b: {
+        cont: 'b',
+        extends: ['a', 'c']
+    },
+    c: {
+        cont: 'c',
+        extends: ['d', 'b']
+    },
+    d: {
+        cont: 'd',
+    },
+    e: {
+        cont: 'e',
+        extends: 'c'
     }
-    test();
-})().catch((err) => Logger.err(err));
+}
+
+const searchHistory = [];
+
+function mergeExtends(t: string) {
+    searchHistory.push(t);
+    const result = [];
+    for (const val of obj[t].extends) {
+        if (searchHistory.includes(val)) {
+            throw `template ${t} has circular dependencies!`;
+            break;
+        } else if (obj[val].extends) {
+            result.push(...mergeExtends(val));
+        } else {
+            result.push(obj[val].cont);
+        }
+    }
+    result.push(obj[t].cont);
+    return result;
+}
+
+Logger.debug(mergeExtends('c'));
