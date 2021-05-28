977e32b61e12e94de21a84721942d41317ef5ecb
Zorin <pikasama@qq.com>
2021-05-29 00:46:29 +0800

    test: extension function test 2

--- src/modules/extend.ts
+++ src/modules/extend.ts
@@ -1,63 +1,41 @@
-import {Logger, mergeMap, Template} from './utils';
+import { Logger, mergeMap, Template, fileMap, TemplateWithExtend } from './utils';
 
-/*
- 模板含有扩展选项才会调用本函数，为避免不必要的类型检查，
- 用一个新接口覆盖Template接口
- */
-interface TemplateWithExtend extends Template {
-    extends: string[];
-}
+async function extension(template: TemplateWithExtend, templateMap: Map<string, Template>) {
+    // 解析含有扩展选项的模板历史，防止循环扩展导致的堆栈溢出
+    const resolveExtendsHistory: Template[] = [];
+    // 解析没有扩展选项的目标历史，防止重复扩展
+    const resolveNormalHistory: Template[] = [];
 
-async function extension(template: TemplateWithExtend , templateMap: Map<string, Template>) {
-    // BUG: 暂不支持嵌套的扩展
-    // let mergeFiles: Template['fileMap'];
-    // const mergeTemplateList = template.extends as string[];
+    // 嵌套扩展解析函数
+    function resolveExtends(templateWithExtend: TemplateWithExtend): Array<fileMap> {
+        resolveExtendsHistory.push(templateWithExtend);
+        const result: Array<fileMap> = [];
 
-    // 解析历史，防止循环扩展导致的堆栈溢出
-    const resolveHistory: Template[] = [];
-
-    // 嵌套扩展解析函数 - 待测试
-    function resolveExtends(template: TemplateWithExtend) {
-        resolveHistory.push(template);
-        const result: TemplateWithExtend['fileMap'][] = [];
-        template.extends.map(val => {
+        templateWithExtend.extends.map((val) => {
             const extendTemplate = templateMap.get(val);
             if (!extendTemplate) {
-                throw `Could not find template ${val}!`
-            } else if (resolveHistory.includes(extendTemplate)) {
-                throw `Template ${val} has circular extends!`
+                throw `Could not find template ${val}!`;
             } else if (extendTemplate.extends) {
+                if (resolveExtendsHistory.includes(extendTemplate)) {
+                    throw `Template ${val} has circular extends!`;
+                }
+
                 result.push(...resolveExtends(extendTemplate as TemplateWithExtend));
             } else {
+                if (resolveNormalHistory.includes(extendTemplate)) {
+                    throw `Template ${templateWithExtend.name} has duplicate extends!`;
+                }
+
+                resolveNormalHistory.push(extendTemplate);
                 result.push(extendTemplate.fileMap);
             }
+
             return undefined;
         });
-        result.push(template.fileMap);
+
+        result.push(templateWithExtend.fileMap);
         return result;
     }
-
-
-    // mergeTemplateList.push(template.name);
-
-    // const firstTemplate = templateMap.get(mergeTemplateList[0]);
-    // if (!firstTemplate) {
-    //     throw `Could not find template ${mergeTemplateList[0]}`;
-    // }
-    //
-    // mergeTemplateList.shift();
-    // mergeFiles = firstTemplate.fileMap;
-    //
-    // mergeTemplateList.map((val) => {
-    //     const templ = templateMap.get(val);
-    //     if (!templ) {
-    //         throw `Could not find template ${val}`;
-    //     }
-    //     mergeFiles = mergeMap(mergeFiles, templ.fileMap);
-    //     return undefined;
-    // });
-    //
-    // Logger.debug(mergeFiles);
 }
 
-export { extension as mergeExtend};
+export { extension as mergeExtend };
