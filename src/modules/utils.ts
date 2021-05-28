55df5526742d4465c6d6972207e2fbaa61383c55
Zorin <pikasama@qq.com>
2021-05-29 00:45:54 +0800

    chore: new type and interface

--- src/modules/utils.ts
+++ src/modules/utils.ts
@@ -17,6 +17,11 @@ interface Template {
     extends?: string[];
 }
 
+// 含有扩展选项的模板
+interface TemplateWithExtend extends Template {
+    extends: string[];
+}
+
 interface ChoiceBox extends copyAttr<ChoiceBox, 'files', 'devDependencies' | 'scripts'> {
     files: string[];
     [index: string]: string[];
@@ -27,6 +32,8 @@ interface PackageJSON {
 }
 
 type ErrTypes = NodeJS.ErrnoException | string | null;
+
+type fileMap = Template['fileMap'];
 
 // await帮助函数，帮助捕获异常
 function awaitHelper<T, U = string>(promise: Promise<T>): Promise<[U | null, T | null]> {
@@ -57,22 +64,41 @@ const Logger: LoggerMethods = {
     },
 };
 
-interface LoggerMethods extends copyAttr<LoggerMethods, 'info', 'done' | 'upd' | 'throw'> {
+interface LoggerMethods extends copyAttr<LoggerMethods, 'info', 'done' | 'upd'> {
     err: (msg: ErrTypes) => void;
     warn: LoggerMethods['err'];
     info: (msg: string) => void;
     debug: (msg: unknown) => void;
     newLine: (lines: number) => void;
+    throw: (msg: string) => never;
 }
 
+/*
+    复制指定对象的键的类型至新的键
+    T: 目标对象
+    K: 目标对象的键
+    N: 新的键
+ */
 type copyAttr<T, K extends keyof T, N extends string> = {
     [P in N]: T[K];
 };
 
-function mergeMap(map1: Template['fileMap'], map2: Template['fileMap']): Template['fileMap'] {
+function mergeMap(map1: fileMap, map2: fileMap): fileMap {
     const arr = [...map1];
     arr.push(...map2);
     return new Map(arr);
 }
 
-export { photinia, Configuration, Template, PackageJSON, Logger, ChoiceBox, awaitHelper, arrayToObject, mergeMap };
+export {
+    photinia,
+    Configuration,
+    Template,
+    TemplateWithExtend,
+    PackageJSON,
+    fileMap,
+    ChoiceBox,
+    awaitHelper,
+    arrayToObject,
+    mergeMap,
+    Logger,
+};
