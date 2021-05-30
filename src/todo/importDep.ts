0b3ef6f97aae5961a906597c9c65aea9fb1e8da3
Zorin <pikasama@qq.com>
2021-05-30 16:45:43 +0800

    chore: delete unused files

--- src/todo/importDep.ts
+++
@@ -1,5 +0,0 @@
-import * as shell from 'shelljs';
-import { PackageJSON } from '../modules/utils';
-import * as prettier from 'prettier';
-const packageInfo: PackageJSON = JSON.parse(shell.cat('../package.json').toString());
-console.log(packageInfo.scripts);
